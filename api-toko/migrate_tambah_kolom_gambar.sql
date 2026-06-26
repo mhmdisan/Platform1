29d4b8ae7d26bf77e7d1329facc6684463eaff2d
mhmdisan <muh.san9488@gmail.com>
2026-06-26 19:21:28 +0700

    Delete api-toko directory

--- api-toko/migrate_tambah_kolom_gambar.sql
+++
@@ -1,7 +0,0 @@
--- Jalankan query ini di phpMyAdmin untuk menambah kolom gambar
--- (Hanya perlu dijalankan SEKALI)
-
-ALTER TABLE barang ADD COLUMN gambar VARCHAR(255) DEFAULT '' AFTER harga;
-
--- Verifikasi:
--- DESCRIBE barang;
